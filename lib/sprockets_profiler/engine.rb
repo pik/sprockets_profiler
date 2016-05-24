3f75f19090ce063afc660f092cbb59a7395433f3
pik <alexander.maznev@gmail.com>
2016-05-24 17:38:41 +0800

    config.assets for SprocketsProfiler::Engine

--- lib/sprockets_profiler/engine.rb
+++ lib/sprockets_profiler/engine.rb
@@ -1,5 +1,7 @@
 module SprocketsProfiler
   class Engine < ::Rails::Engine
     isolate_namespace SprocketsProfiler
+    config.assets.raise_runtime_errors = false
+    config.assets.precompile << 'd3.js' << 'd3-tip.js' << 'd3.flameGraph.js'
   end
 end
